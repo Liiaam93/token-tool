11d17408597461aa6ca19284f24e30e316acb547
Liam Burbidge <81435487+Liiaam93@users.noreply.github.com>
2024-06-26 21:03:05 +0100

    Feature Update
    Added API request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Textarea,
   Button,
@@ -9,14 +9,41 @@ import {
   Checkbox,
 } from "@chakra-ui/react";
 import MessageDisplay from "./components/MessageDisplay";
+import { fetchPage } from "./scrape";
 
 interface AppProps {}
 
+export interface FormattedBarcode {
+  barcode: string;
+  originalBarcode: string;
+  valid: boolean;
+}
+
+export interface PortalType {
+  created_by: string;
+  created_date: number;
+  created_date_string: string;
+  email: string;
+  id: string;
+  modified_time: string;
+  order_open: boolean;
+  order_search_id: string;
+  order_type: string;
+  pharmacy_account_number: string;
+  pharmacy_name: string;
+  pharmacy_post_code: string;
+  record_status: string;
+  record_type: string;
+}
+
 const App: React.FC<AppProps> = () => {
   const [value, setValue] = useState("");
-  const [formattedBarcodes, setFormattedBarcodes] = useState<string[]>([]);
+  const [formattedBarcodes, setFormattedBarcodes] = useState<
+    FormattedBarcode[]
+  >([]);
   const [selectedTokens, setSelectedTokens] = useState<string[]>([]);
   const [copiedIndex, setCopiedIndex] = useState<number | null>(null);
+  const [portalData, setPortalData] = useState<PortalType[]>([]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const inputValue = e.target.value;
@@ -36,16 +63,30 @@ const App: React.FC<AppProps> = () => {
     setCopiedIndex(index);
   };
 
+  useEffect(() => {
+    const fetchPortal = async () => {
+      const { data } = await fetchPage();
+      console.log(data.items);
+      setPortalData(data.items);
+    };
+    fetchPortal();
+  }, []);
+
   const handleClick = () => {
     setCopiedIndex(null);
+    setSelectedTokens([]);
 
     const barcodes = value.split(/\s+/);
     const formattedBarcodes = barcodes
       .map((barcode) => {
         const trimmedBarcode = barcode.replace(/[^A-Za-z0-9+]/g, "");
-        return trimmedBarcode.slice(0, 18); // Trim to 18 characters
+        return {
+          barcode: trimmedBarcode.slice(0, 18),
+          valid: trimmedBarcode.length === 18,
+          originalBarcode: barcode,
+        };
       })
-      .filter((barcode) => barcode.length >= 16) as string[]; // Type assertion to string[]
+      .filter(({ barcode }) => barcode.length >= 16) as FormattedBarcode[]; // Type assertion to string[]
 
     setFormattedBarcodes(formattedBarcodes);
   };
@@ -98,62 +139,63 @@ const App: React.FC<AppProps> = () => {
         </Box>
 
         <Box flex="1" m="2">
-          {formattedBarcodes.map((barcode, index) => (
-            <Flex
-              key={index}
-              align="center"
-              mb={1}
-              maxH="5vh"
-              border="solid 1px white"
-              borderRadius="5"
-              backgroundColor={
-                barcode.length < 18 ? "red.900" : "whiteAlpha.200"
-              }
-              _hover={
-                barcode.length < 18
-                  ? { bg: "red.700" }
-                  : { bg: "whiteAlpha.400" }
-              }
-            >
-              <Text
-                flex="1"
-                marginY="0"
-                ml="10"
-                textAlign="center"
-                fontSize="12"
-                color="white"
-              >
-                {barcode.length === 18
-                  ? barcode.replace(/(.{6})(.{6})(.{6})/, "$1-$2-$3")
-                  : barcode + " - Token Invalid"}
-              </Text>
-              <Button
-                m="5"
-                size="xs"
-                colorScheme={copiedIndex === index ? "green" : "whiteAlpha"}
-                onClick={() =>
-                  copyToClipboard(
-                    barcode.replace(/(.{6})(.{6})(.{6})/, "$1-$2-$3"),
-                    index
-                  )
+          {formattedBarcodes.map(({ barcode, valid }, index) => {
+            const renderBarcode = barcode
+              .toUpperCase()
+              .replace(/(.{6})(.{6})(.{6})/, "$1-$2-$3");
+            return (
+              <Flex
+                key={index}
+                align="center"
+                mb={1}
+                maxH="5vh"
+                border="solid 1px white"
+                borderRadius="5"
+                backgroundColor={!valid ? "red.900" : "whiteAlpha.200"}
+                _hover={
+                  barcode.length < 18
+                    ? { bg: "red.700" }
+                    : { bg: "whiteAlpha.400" }
                 }
               >
-                {copiedIndex === index ? "Copied" : "Copy"}
-              </Button>
-              <Checkbox
-                isChecked={selectedTokens.includes(barcode)}
-                onChange={() => handleCheckboxChange(barcode)}
-                mr={2}
-                size="sm"
-                colorScheme="red"
-                color="white"
-              >
-                RTS
-              </Checkbox>
-            </Flex>
-          ))}
+                <Text
+                  flex="1"
+                  marginY="0"
+                  ml="10"
+                  textAlign="center"
+                  fontSize="12"
+                  color="white"
+                >
+                  {`${renderBarcode}${!valid ? " - Token Invalid" : ""}`}
+                </Text>
+                <Button
+                  m="5"
+                  size="xs"
+                  colorScheme={copiedIndex === index ? "green" : "whiteAlpha"}
+                  onClick={() => copyToClipboard(renderBarcode, index)}
+                >
+                  {copiedIndex === index ? "Copied" : "Copy"}
+                </Button>
+                <Checkbox
+                  isChecked={selectedTokens.includes(barcode)}
+                  onChange={() => handleCheckboxChange(barcode)}
+                  mr={2}
+                  size="sm"
+                  colorScheme="red"
+                  color="white"
+                >
+                  RTS
+                </Checkbox>
+              </Flex>
+            );
+          })}
         </Box>
       </Flex>
+      {portalData.map((p: PortalType, index) => (
+        <Text color={"white"} key={index}>
+          {p.pharmacy_account_number}
+        </Text>
+      ))}
     </Box>
   );
 };
