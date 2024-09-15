45771121bffa992ca63023bb9bd98a7f42c4067e
Liam Burbidge <81435487+Liiaam93@users.noreply.github.com>
2024-09-15 22:47:55 +0100

    Feature
    Script Numbers

---
+++ src/components/BarcodeItem.tsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import { Flex, Text, Button, Checkbox, Input } from "@chakra-ui/react";
+import { FormattedBarcode } from "../pages/Home";
+
+interface BarcodeItemProps {
+  barcodeData: FormattedBarcode;
+  showScriptInput: boolean;
+  updateScriptNumber: (barcode: string, scriptNumber: string) => void;
+  onRTSChange: (barcode: string, checked: boolean) => void; // RTS change handler passed from Home
+  isChecked: boolean; // Control the checked state
+  lastCopiedBarcode: string; // Pass this prop to track last copied barcode
+  setLastCopiedBarcode: (barcode: string) => void; // Add this prop
+}
+
+const BarcodeItem: React.FC<BarcodeItemProps> = ({
+  barcodeData,
+  showScriptInput,
+  updateScriptNumber,
+  onRTSChange,
+  isChecked,
+  lastCopiedBarcode,
+  setLastCopiedBarcode,
+}) => {
+  const [copied, setCopied] = useState("");
+  const [scriptNumber, setScriptNumber] = useState("");
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(barcodeData.barcode);
+    setLastCopiedBarcode(barcodeData.barcode);
+  };
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onRTSChange(barcodeData.barcode, e.target.checked);
+  };
+
+  const handleScriptNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setScriptNumber(value);
+    updateScriptNumber(barcodeData.barcode, value);
+  };
+
+  const renderBarcode = barcodeData.barcode
+    .toUpperCase()
+    .replace(/(.{6})(.{6})(.{6})/, "$1-$2-$3");
+
+  useEffect(() => {
+    if (lastCopiedBarcode && lastCopiedBarcode !== barcodeData.barcode) {
+      setCopied("");
+    } else if (lastCopiedBarcode === barcodeData.barcode) {
+      setCopied("Copied");
+    }
+  }, [lastCopiedBarcode]);
+
+  return (
+    <Flex
+      m="2"
+      align="center"
+      mb={1}
+      maxH="7vh"
+      border="solid 1px white"
+      borderRadius="5"
+      backgroundColor={!barcodeData.valid ? "red.900" : "whiteAlpha.200"}
+    >
+      <Button
+        m="2"
+        colorScheme="blue"
+        size="xs"
+        onClick={() =>
+          window.open(
+            `https://portal2.national.ncrs.nhs.uk/prescriptionsadmin/prescription?id=${renderBarcode}&instance=1`,
+            "_blank"
+          )
+        }
+      >
+        NHS
+      </Button>
+
+      <Text flex="1" ml="10" textAlign="center" fontSize="12" color="white">
+        {`${renderBarcode}${!barcodeData.valid ? "\n- Invalid" : ""}`}
+      </Text>
+
+      <Button
+        m="5"
+        size="xs"
+        colorScheme={copied ? "green" : "whiteAlpha"}
+        onClick={copyToClipboard}
+      >
+        {copied ? "Copied" : "Copy"}
+      </Button>
+
+      <Checkbox
+        isChecked={isChecked}
+        onChange={handleCheckboxChange}
+        mr={2}
+        size="sm"
+        colorScheme="red"
+        color="white"
+      >
+        RTS
+      </Checkbox>
+
+      {showScriptInput && (
+        <Input
+          color="white"
+          size="xs"
+          placeholder="Script Number"
+          value={scriptNumber}
+          onChange={handleScriptNumberChange}
+          maxW="100px"
+          ml="2"
+          mr="2"
+        />
+      )}
+    </Flex>
+  );
+};
+
+export default BarcodeItem;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Textarea, Button, Box, Flex, Text, Checkbox } from "@chakra-ui/react";
+import { Textarea, Button, Box, Flex, useToast } from "@chakra-ui/react";
 import MessageDisplay from "../components/MessageDisplay";
+import BarcodeItem from "../components/BarcodeItem";
 
 export interface FormattedBarcode {
   barcode: string;
@@ -14,30 +15,23 @@ const Home: React.FC = () => {
     FormattedBarcode[]
   >([]);
   const [selectedTokens, setSelectedTokens] = useState<string[]>([]);
-  const [copiedIndex, setCopiedIndex] = useState<number | null>(null);
+  const [scriptNumbers, setScriptNumbers] = useState<{
+    [barcode: string]: string;
+  }>({});
+  const [showScriptInput, setShowScriptInput] = useState(false);
+  const [invalidBarcodes, setInvalidBarcodes] = useState<string[]>([]);
+  const [lastCopiedBarcode, setLastCopiedBarcode] = useState<string>("");
+
+  // Initialize useToast
+  const toast = useToast();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const inputValue = e.target.value;
     setValue(inputValue);
   };
 
-  const handleCheckboxChange = (barcode: string) => {
-    setSelectedTokens((prevTokens) =>
-      prevTokens.includes(barcode)
-        ? prevTokens.filter((token) => token !== barcode)
-        : [...prevTokens, barcode]
-    );
-  };
-
-  const copyToClipboard = (barcode: string, index: number) => {
-    navigator.clipboard.writeText(barcode);
-    setCopiedIndex(index);
-  };
-
   const handleClick = () => {
-    setCopiedIndex(null);
     setSelectedTokens([]);
-
     const barcodes = value.split(/\s+/);
     const formattedBarcodes = barcodes
       .map((barcode) => {
@@ -50,19 +44,68 @@ const Home: React.FC = () => {
       })
       .filter(({ barcode }) => barcode.length >= 16) as FormattedBarcode[];
 
+    const invalids = formattedBarcodes.filter(({ valid }) => !valid);
+    setInvalidBarcodes(invalids.map(({ originalBarcode }) => originalBarcode));
+
     setFormattedBarcodes(formattedBarcodes);
+  };
+
+  const handleAddScriptNumbers = () => {
+    setShowScriptInput((prev) => !prev);
+  };
+
+  // Function to handle script number updates from BarcodeItem
+  const updateScriptNumber = (barcode: string, scriptNumber: string) => {
+    setScriptNumbers((prev) => ({
+      ...prev,
+      [barcode]: scriptNumber,
+    }));
+  };
+
+  // Function to handle RTS checkbox changes
+  const handleRTSChange = (barcode: string, checked: boolean) => {
+    setSelectedTokens((prevTokens) =>
+      checked
+        ? [...prevTokens, barcode]
+        : prevTokens.filter((token) => token !== barcode)
+    );
+  };
+
+  // Function to copy text to clipboard
+  const copyToClipboard = () => {
+    const formattedText = formattedBarcodes
+      .map(
+        ({ barcode }) =>
+          `${barcode
+            .toUpperCase()
+            .replace(/(.{6})(.{6})(.{6})/, "$1-$2-$3")} - (REF: ${
+            scriptNumbers[barcode] || ""
+          })`
+      )
+      .join("\n");
+
+    navigator.clipboard.writeText(formattedText).then(() => {
+      toast({
+        title: "Copied to clipboard",
+        description:
+          "The formatted barcodes and script numbers have been copied.",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    });
   };
 
   return (
     <Box bg="gray.800" minHeight="100vh">
       <Flex
         p={2}
-        maxW="80vw"
+        maxW="90vw"
         m="auto"
         border="solid white 2px"
         borderRadius="5"
       >
-        <Box flex="1" mr={4}>
+        <Box flex="1" mr={4} justifyContent={"center"}>
           <Textarea
             color="white"
             placeholder="Paste barcodes here..."
@@ -72,91 +115,83 @@ const Home: React.FC = () => {
             h="60vh"
             m="2"
           />
-          <Box display="flex" flexDirection="column" justifyContent="center">
-            <Button
-              color="black"
-              colorScheme="green"
-              onClick={handleClick}
-              m="2"
-              width="90%"
-            >
-              Format Barcodes
-            </Button>
+          <Button colorScheme="green" onClick={handleClick} m="2" width="90%">
+            Format Barcodes
+          </Button>
 
-            <MessageDisplay
-              selectedTokens={selectedTokens}
-              formattedBarcodes={formattedBarcodes}
-            />
-          </Box>
+          <MessageDisplay
+            selectedTokens={selectedTokens}
+            formattedBarcodes={formattedBarcodes}
+          />
         </Box>
 
         <Box flex="1" m="2">
-          {formattedBarcodes.map(({ barcode, valid }, index) => {
-            const renderBarcode = barcode
-              .toUpperCase()
-              .replace(/(.{6})(.{6})(.{6})/, "$1-$2-$3");
-            return (
-              <Flex
-                key={index}
-                align="center"
-                mb={1}
-                maxH="5vh"
-                border="solid 1px white"
-                borderRadius="5"
-                backgroundColor={!valid ? "red.900" : "whiteAlpha.200"}
-                _hover={
-                  barcode.length < 18
-                    ? { bg: "red.700" }
-                    : { bg: "whiteAlpha.400" }
-                }
-              >
-                <Button
-                  colorScheme="blue"
-                  size="xs"
-                  onClick={() =>
-                    window.open(
-                      `https://portal2.national.ncrs.nhs.uk/prescriptionsadmin/prescription?id=${renderBarcode}&instance=1`,
-                      "_blank"
-                    )
-                  }
-                >
-                  NHS
-                </Button>
-                <Text
-                  flex="1"
-                  marginY="0"
-                  ml="10"
-                  textAlign="center"
-                  fontSize="12"
-                  color="white"
-                >
-                  {`${renderBarcode}${!valid ? " - Token Invalid" : ""}`}
-                </Text>
-                <Button
-                  m="5"
-                  size="xs"
-                  colorScheme={copiedIndex === index ? "green" : "whiteAlpha"}
-                  onClick={() => copyToClipboard(renderBarcode, index)}
-                >
-                  {copiedIndex === index ? "Copied" : "Copy"}
-                </Button>
+          {invalidBarcodes.length > 0 && (
+            <Box
+              m="2"
+              w="100%"
+              color="red.500"
+              textAlign={"center"}
+              fontSize={"sm"}
+              textDecoration={"underline"}
+            >
+              {`Invalid barcode(s) detected - please double check, as this could be a false error `}
+            </Box>
+          )}
+          <Flex justifyContent="center" mb="4">
+            <Button
+              colorScheme="green"
+              size="sm"
+              onClick={handleAddScriptNumbers}
+            >
+              {showScriptInput ? "Hide Script Numbers" : "Add Script Numbers"}
+            </Button>
+          </Flex>
 
-                <Checkbox
-                  isChecked={selectedTokens.includes(barcode)}
-                  onChange={() => handleCheckboxChange(barcode)}
-                  mr={2}
-                  size="sm"
-                  colorScheme="red"
-                  color="white"
-                >
-                  RTS
-                </Checkbox>
-              </Flex>
-            );
-          })}
+          {formattedBarcodes.map((barcodeData, index) => (
+            <BarcodeItem
+              key={index}
+              barcodeData={barcodeData}
+              showScriptInput={showScriptInput}
+              updateScriptNumber={updateScriptNumber}
+              onRTSChange={handleRTSChange}
+              isChecked={selectedTokens.includes(barcodeData.barcode)}
+              lastCopiedBarcode={lastCopiedBarcode}
+              setLastCopiedBarcode={setLastCopiedBarcode}
+            />
+          ))}
+
+          {Object.keys(scriptNumbers).length > 0 && (
+            <Box mt="4">
+              <Textarea
+                mt="2"
+                value={formattedBarcodes
+                  .map(
+                    ({ barcode }) =>
+                      `${barcode
+                        .toUpperCase()
+                        .replace(/(.{6})(.{6})(.{6})/, "$1-$2-$3")} - ${
+                        scriptNumbers[barcode] || "still with ordering team"
+                      }`
+                  )
+                  .join("\n")}
+                isReadOnly
+                color="white"
+              />
+              <Button
+                colorScheme="blue"
+                mt="2"
+                w="100%"
+                onClick={copyToClipboard}
+              >
+                Copy to Clipboard
+              </Button>
+            </Box>
+          )}
         </Box>
       </Flex>
     </Box>
   );
 };
+
 export default Home;
