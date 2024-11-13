af1029fdb50aa0f220e2cae7af19ff42a26ee754
Liam Burbidge <81435487+Liiaam93@users.noreply.github.com>
2024-11-13 14:51:51 +0000

    Update fetchPortal.tsx

--- src/utils/fetchPortal.tsx
+++ src/utils/fetchPortal.tsx
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export const fetchPortal = (token: string, statusFilter: string) => {
   return axios.get(
-    
+    `
   https://vfgar9uinc.execute-api.eu-west-2.amazonaws.com/prod/fp/order?${new URLSearchParams(
     {
       pageSize: "100",
       page: "1",
     }
-  ).toString()}&recordStatus=${statusFilter},
+  ).toString()}&recordStatus=${statusFilter}`,
     {
       headers: {
         Authorization: token,
