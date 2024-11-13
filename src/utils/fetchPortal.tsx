310814eb045b1fc752d2ff4b95c3ca4d900f06ee
Liam Burbidge <81435487+Liiaam93@users.noreply.github.com>
2024-11-13 13:52:46 +0000

    Update fetchPortal.tsx

--- src/utils/fetchPortal.tsx
+++ src/utils/fetchPortal.tsx
@@ -1,18 +1,24 @@
 import axios from "axios";
 
 export const fetchPortal = (token: string, statusFilter: string) => {
-  return axios.get(
-    `
-  https://vfgar9uinc.execute-api.eu-west-2.amazonaws.com/prod/fp/order?${new URLSearchParams(
-    {
-      pageSize: "100",
-      page: "1",
-    }
-  ).toString()}&recordStatus=${statusFilter}`,
-    {
-      headers: {
-        Authorization: token,
-      },
-    }
-  );
+  // Start with the common query parameters
+  const queryParams: Record<string, string> = {
+    pageSize: "100",
+    page: "1",
+  };
+
+  // Add the recordStatus parameter only if it's not an empty string
+  if (statusFilter !== '') {
+    queryParams.recordStatus = statusFilter;
+  }
+
+  // Construct the URL with the query parameters
+  const url = `https://vfgar9uinc.execute-api.eu-west-2.amazonaws.com/prod/fp/order?${new URLSearchParams(queryParams).toString()}`;
+
+  // Make the GET request
+  return axios.get(url, {
+    headers: {
+      Authorization: token,
+    },
+  });
 };
