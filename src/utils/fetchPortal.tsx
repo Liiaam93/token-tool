497ffcaa4ebbd64e04cfebd7cdbab1c6b1132fb7
Liam Burbidge <81435487+Liiaam93@users.noreply.github.com>
2024-11-14 14:13:38 +0000

    Update fetchPortal.tsx

--- src/utils/fetchPortal.tsx
+++ src/utils/fetchPortal.tsx
@@ -5,7 +5,7 @@ export const fetchPortal = (token: string, statusFilter: string) => {
     `
   https://vfgar9uinc.execute-api.eu-west-2.amazonaws.com/prod/fp/order?${new URLSearchParams(
     {
-      pageSize: "200",
+      pageSize: "100",
       page: "1",
     }
   ).toString()}&recordStatus=${statusFilter}`,
