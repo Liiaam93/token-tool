45bd3ecd56ac14bed6e912c14c0486281b9a9b08
Liam Burbidge <81435487+Liiaam93@users.noreply.github.com>
2025-06-30 14:03:19 +0000

    branch

---
+++ src/data/headlines.ts
@@ -0,0 +1,5 @@
+export const Headlines = [
+    'Breaking News: NHS Spine Admits It Needs a Chiropractor', 
+    'Lorry driver claims his prescription has travelled further than him this week.'
+
+]
